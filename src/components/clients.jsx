df17757044499b6f239588faee2c9bee2bde23eb
hikeislife <hikeislife@users.noreply.github.com>
2019-07-14 16:25:20 +0200

    static client list, css grid

--- src/components/clients.jsx
+++ src/components/clients.jsx
@@ -4,7 +4,24 @@ class Clients extends Component {
 	//state = {}
 
 	render() {
-		return ( <section className="clients" id="clients">klijenti</section> )
+		return ( 
+			<section className="clients" id="clients">
+				<img src="/imgs/clients/client-placeholder-01.jpg" alt="client" className="client-narrow" />
+				<img src="/imgs/clients/client-placeholder-02.jpg" alt="client" className="client-narrow" />
+				<img src="/imgs/clients/client-placeholder-03.jpg" alt="client" className="client-narrow" />
+				<img src="/imgs/clients/client-placeholder-04.jpg" alt="client" className="client-wide" />
+				<img src="/imgs/clients/client-placeholder-05.jpg" alt="client" className="client-narrow" />
+				<img src="/imgs/clients/client-placeholder-01.jpg" alt="client" className="client-narrow" />
+				<img src="/imgs/clients/client-placeholder-02.jpg" alt="client" className="client-wide" />
+				<img src="/imgs/clients/client-placeholder-03.jpg" alt="client" className="client-narrow" />
+				<img src="/imgs/clients/client-placeholder-04.jpg" alt="client" className="client-narrow" />
+				<img src="/imgs/clients/client-placeholder-05.jpg" alt="client" className="client-narrow" />
+				<img src="/imgs/clients/client-placeholder-01.jpg" alt="client" className="client-wide" />
+				<img src="/imgs/clients/client-placeholder-02.jpg" alt="client" className="client-narrow" />
+				<img src="/imgs/clients/client-placeholder-03.jpg" alt="client" className="client-narrow" />
+				<img src="/imgs/clients/client-placeholder-04.jpg" alt="client" className="client-narrow" />
+				<img src="/imgs/clients/client-placeholder-05.jpg" alt="client" className="client-narrow" />
+			</section> )
 	}
 }
 export default Clients;
