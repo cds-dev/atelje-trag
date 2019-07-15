afc3236576f21278a4fc5697aa9ffc3ff95173fb
hikeislife <hikeislife@users.noreply.github.com>
2019-07-16 01:24:08 +0200

    Updates

---
+++ precache-manifest.cae6b090f484e37525ccb8b03e8c5ac2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d8b952ac6afb8b7f4c2e5528968c9e7",
+    "url": "/atelje-trag/index.html"
+  },
+  {
+    "revision": "32fe5729ae4e63318319",
+    "url": "/atelje-trag/static/css/main.94b65e79.chunk.css"
+  },
+  {
+    "revision": "6e4b86a4c08415a39a8d",
+    "url": "/atelje-trag/static/js/2.d3e4bf28.chunk.js"
+  },
+  {
+    "revision": "32fe5729ae4e63318319",
+    "url": "/atelje-trag/static/js/main.a853954f.chunk.js"
+  },
+  {
+    "revision": "50f24a827b210209fe02",
+    "url": "/atelje-trag/static/js/runtime~main.45976598.js"
+  }
+]);
