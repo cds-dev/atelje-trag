331fb12587c11ad9a03a4cb0dab60f073a331504
hikeislife <hikeislife@users.noreply.github.com>
2019-07-15 19:07:19 +0200

    app to class

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './scss/App.scss';
 import Video from './components/video'
 import { Route, Switch } from 'react-router-dom';
@@ -6,21 +6,19 @@ import Home from './components/home';
 import People from './components/people';
 import Machines from './components/machines';
 
-function App() {
+// function App() {
+class App extends Component {
+  render() {
   return (
     <div className="App">
-      {/*<header className="App-header">*/}
         <Video/>
-      {/*</header>*/}
-      <div className="content onTop"> 
 			<Switch>
 				<Route path="/machines" component={Machines} />
 				<Route path="/people" component={People} />
 				<Route path="/" component={Home} />
 			</Switch>
-		</div>
     </div>
   );
-}
+}}
 
 export default App;
