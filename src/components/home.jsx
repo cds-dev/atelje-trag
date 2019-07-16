99b7591b3049a7ee94159dbc0a9c0409df1161ba
hikeislife <hikeislife@users.noreply.github.com>
2019-07-16 10:44:13 +0200

    nav urls

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -15,8 +15,8 @@ class Home extends Component {
 			<div className="main">
 				<div className="fp_button lang lang_en" onClick={this.props.setEng}>EN</div>
 				<div className="fp_button lang lang_sr" onClick={this.props.setSer}>SR</div>
-				<Link to='/people'><div className="fp_button sect sect_peop">{lang[language][0]}</div></Link>
-				<Link to='/machines'><div className="fp_button sect sect_mach">{lang[language][1]}</div></Link>
+				<Link to='./people'><div className="fp_button sect sect_peop">{lang[language][0]}</div></Link>
+				<Link to='./machines'><div className="fp_button sect sect_mach">{lang[language][1]}</div></Link>
 			</div> )
 	}
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -19,12 +19,12 @@ class NavBar extends Component {
 
 		return ( <nav><ul>
 			<div className="mainNav">
-				<Link to='/'><li>{item1[lang]}</li></Link>
-				<Link to='/people#aboutus'><li>{item2[lang]}</li></Link>
-				<Link to='/machines#aboutstudio'><li>{item3[lang]}</li></Link>
-				<Link to='/people#clients'><li>{item4[lang]}</li></Link>
-				<Link to='/machines#equipment'><li>{item5[lang]}</li></Link>
-				<Link to='/people#contact'><li>{item6[lang]}</li></Link>
+				<Link to='./'><li>{item1[lang]}</li></Link>
+				<Link to='./people#aboutus'><li>{item2[lang]}</li></Link>
+				<Link to='./machines#aboutstudio'><li>{item3[lang]}</li></Link>
+				<Link to='./people#clients'><li>{item4[lang]}</li></Link>
+				<Link to='./machines#equipment'><li>{item5[lang]}</li></Link>
+				<Link to='./people#contact'><li>{item6[lang]}</li></Link>
 			</div>
 			<div className="languages">
 				<li onClick={this.props.setSer}>SR</li>
