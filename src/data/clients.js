14dc85c8e4fad7b8d0bd620a645e209f210c236f
hikeislife <hikeislife@users.noreply.github.com>
2019-07-16 10:10:21 +0200

    image urls

--- src/data/clients.js
+++ src/data/clients.js
@@ -1,196 +1,196 @@
 export const clients = [
  {
   _id : "001",
-  img : "/imgs/clients/client-placeholder-01.jpg",
+  img : "./imgs/clients/client-placeholder-01.jpg",
   alt : "",
   cls : "client-narrow",
   txt : ""
  },
  {
   _id : "002",
-  img : "/imgs/clients/client-placeholder-02.jpg",
+  img : "./imgs/clients/client-placeholder-02.jpg",
   alt : "",
   cls : "client-wide",
   txt : ""
  },
  {
   _id : "003",
-  img : "/imgs/clients/client-placeholder-03.jpg",
+  img : "./imgs/clients/client-placeholder-03.jpg",
   alt : "",
   cls : "client-narrow",
   txt : ""
  },
  {
   _id : "004",
-  img : "/imgs/clients/client-placeholder-04.jpg",
+  img : "./imgs/clients/client-placeholder-04.jpg",
   alt : "",
   cls : "client-wide",
   txt : "hfdjsk dhsjklhf dkfh"
  },
  {
   _id : "005",
-  img : "/imgs/clients/client-placeholder-05.jpg",
+  img : "./imgs/clients/client-placeholder-05.jpg",
   alt : "",
   cls : "client-narrow",
   txt : ""
  },
  {
   _id : "006",
-  img : "/imgs/clients/client-placeholder-01.jpg",
+  img : "./imgs/clients/client-placeholder-01.jpg",
   alt : "",
   cls : "client-wide",
   txt : "hfdsjkl hfdksjlhf dkjhfksljld"
  },
  {
   _id : "007",
-  img : "/imgs/clients/client-placeholder-02.jpg",
+  img : "./imgs/clients/client-placeholder-02.jpg",
   alt : "",
   cls : "client-narrow",
   txt : ""
  },
  {
   _id : "008",
-  img : "/imgs/clients/client-placeholder-03.jpg",
+  img : "./imgs/clients/client-placeholder-03.jpg",
   alt : "",
   cls : "client-narrow",
   txt : ""
  },
  {
   _id : "009",
-  img : "/imgs/clients/client-placeholder-04.jpg",
+  img : "./imgs/clients/client-placeholder-04.jpg",
   alt : "",
   cls : "client-wide",
   txt : "hfjkdls jdslhfjdsh sjdbfkl hfdsl"
  },
  {
   _id : "010",
-  img : "/imgs/clients/client-placeholder-05.jpg",
+  img : "./imgs/clients/client-placeholder-05.jpg",
   alt : "",
   cls : "client-narrow",
   txt : ""
  },
  {
   _id : "011",
-  img : "/imgs/clients/client-placeholder-01.jpg",
+  img : "./imgs/clients/client-placeholder-01.jpg",
   alt : "",
   cls : "client-narrow",
   txt : ""
  },
  {
   _id : "012",
-  img : "/imgs/clients/client-placeholder-02.jpg",
+  img : "./imgs/clients/client-placeholder-02.jpg",
   alt : "",
   cls : "client-narrow",
   txt : ""
  },
  {
   _id : "013",
-  img : "/imgs/clients/client-placeholder-03.jpg",
+  img : "./imgs/clients/client-placeholder-03.jpg",
   alt : "",
   cls : "client-narrow",
   txt : ""
  },
  {
   _id : "014",
-  img : "/imgs/clients/client-placeholder-04.jpg",
+  img : "./imgs/clients/client-placeholder-04.jpg",
   alt : "",
   cls : "client-narrow",
   txt : ""
  },
  {
   _id : "015",
-  img : "/imgs/clients/client-placeholder-01.jpg",
+  img : "./imgs/clients/client-placeholder-01.jpg",
   alt : "",
   cls : "client-narrow",
   txt : ""
  },
  {
   _id : "016",
-  img : "/imgs/clients/client-placeholder-02.jpg",
+  img : "./imgs/clients/client-placeholder-02.jpg",
   alt : "",
   cls : "client-narrow",
   txt : ""
  },
  {
   _id : "017",
-  img : "/imgs/clients/client-placeholder-03.jpg",
+  img : "./imgs/clients/client-placeholder-03.jpg",
   alt : "",
   cls : "client-narrow",
   txt : ""
  },
  {
   _id : "018",
-  img : "/imgs/clients/client-placeholder-04.jpg",
+  img : "./imgs/clients/client-placeholder-04.jpg",
   alt : "",
   cls : "client-wide",
   txt : "hfdjsk dhsjklhf dkfh"
  },
  {
   _id : "019",
-  img : "/imgs/clients/client-placeholder-05.jpg",
+  img : "./imgs/clients/client-placeholder-05.jpg",
   alt : "",
   cls : "client-wide",
   txt : ""
  },
  {
   _id : "020",
-  img : "/imgs/clients/client-placeholder-01.jpg",
+  img : "./imgs/clients/client-placeholder-01.jpg",
   alt : "",
   cls : "client-narrow",
   txt : "hfdsjkl hfdksjlhf dkjhfksljld"
  },
  {
   _id : "021",
-  img : "/imgs/clients/client-placeholder-02.jpg",
+  img : "./imgs/clients/client-placeholder-02.jpg",
   alt : "",
   cls : "client-narrow",
   txt : ""
  },
  {
   _id : "022",
-  img : "/imgs/clients/client-placeholder-03.jpg",
+  img : "./imgs/clients/client-placeholder-03.jpg",
   alt : "",
   cls : "client-narrow",
   txt : ""
  },
  {
   _id : "023",
-  img : "/imgs/clients/client-placeholder-04.jpg",
+  img : "./imgs/clients/client-placeholder-04.jpg",
   alt : "",
   cls : "client-wide",
   txt : "hfjkdls jdslhfjdsh sjdbfkl hfdsl"
  },
  {
   _id : "024",
-  img : "/imgs/clients/client-placeholder-05.jpg",
+  img : "./imgs/clients/client-placeholder-05.jpg",
   alt : "",
   cls : "client-wide",
   txt : ""
  },
  {
   _id : "025",
-  img : "/imgs/clients/client-placeholder-01.jpg",
+  img : "./imgs/clients/client-placeholder-01.jpg",
   alt : "",
   cls : "client-narrow",
   txt : ""
  },
  {
   _id : "026",
-  img : "/imgs/clients/client-placeholder-02.jpg",
+  img : "./imgs/clients/client-placeholder-02.jpg",
   alt : "",
   cls : "client-narrow",
   txt : ""
  },
  {
   _id : "027",
-  img : "/imgs/clients/client-placeholder-03.jpg",
+  img : "./imgs/clients/client-placeholder-03.jpg",
   alt : "",
   cls : "client-wide",
   txt : ""
  },
  {
   _id : "028",
-  img : "/imgs/clients/client-placeholder-04.jpg",
+  img : "./imgs/clients/client-placeholder-04.jpg",
   alt : "",
   cls : "client-narrow",
   txt : ""
