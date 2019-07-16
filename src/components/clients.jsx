f18528dd58707ccd2e913e408e000d49186aa9d2
hikeislife <hikeislife@users.noreply.github.com>
2019-07-16 17:12:00 +0200

    safari grid vs flex clients test

--- src/components/clients.jsx
+++ src/components/clients.jsx
@@ -16,7 +16,7 @@ class Clients extends Component {
 		//let lang = this.props.lang;
 		return ( 
 			<section className="clients" id="clients">
-				{ this.state.clientList.map( cl => <img src={cl.img} alt={cl.alt} className={cl.cls} key={cl._id} />)}
+				{ this.state.clientList.map( cl => <div className="safaritest"><img src={cl.img} alt={cl.alt} className={cl.cls} key={cl._id} /></div>)}
 			</section> )
 	}
 }
