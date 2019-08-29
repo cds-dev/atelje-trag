10b6c85a8e0689ac20c31ae6089c36d03458fd2f
IgorMilatovic <igby.veko@gmail.com>
2019-08-29 11:02:33 +0200

    da bih pulovao..

--- src/components/clients.jsx
+++ src/components/clients.jsx
@@ -5,15 +5,13 @@ import Gallery from "react-photo-gallery";
 
 class Clients extends Component {
 	state = {
-		clientList: []
+		clientList: [],
 	}
 
 	componentDidMount = () => {
 		this.setState({
 			clientList: getClients()
 		});
-		
-
 	}
 
 	openModal = (e) => {
@@ -33,46 +31,27 @@ class Clients extends Component {
 	}
 
 	componentDidUpdate = () => {
-		//const kliks = document.querySelector('.clent-wide');
-		const clickables = document.querySelectorAll('img');
-		//while(kliks.length === 0) console.log("nothere yet")
+		const clickables = document.querySelector(".react-photo-gallery--gallery");
 		console.log(clickables)
-		// const all = [...clickables]
-		// const just = all[0].parentElement.firstElementChild.ownerDocument.images
-		// console.log([...all[0].parentElement.firstElementChild.ownerDocument.images])
-		// for(let i = 0; i < all.length; i++) {
-		// 	console.log([...clickables][3].parentElement.firstElementChild.ownerDocument.images[i].classList[0]);
-		// 	if([...clickables][3].parentElement.firstElementChild.ownerDocument.images[i].className === 'client-wide') {
-		// 		console.log(i)
-		// 	}
-			
-		// }
-		// [...clickables].forEach(x => {
-
-		// })
-		// console.log([...clickables][3].parentElement.firstElementChild.ownerDocument.images[3]);
-		// //clickables.forEach(x => console.log('test'));
-		// //console.log([clickables])
-		// console.log("dsc")
-		
 	}
 
 	render() {
-
+		
 		//let lang = this.props.lang;
 		return (
 			//<>
-				<section id="clients" ><span>
-				<Gallery photos={this.state.clientList} direction={"column"} onClick={this.openModal} /></span>
+				<section id="clients" >
+				<span><Gallery photos={this.state.clientList} direction={"column"} onClick={this.openModal} /></span>
 				{this.state.clientList.map(cl => <div
 					className="modal"
 					id={'mod'+cl.id}
 					key={cl.id}
 					//style={{background:`url(`+cl.src+`)`}}
 					onClick={this.closeModal}>
-					<img src={cl.src} alt={cl.alt} />
+					<img src={cl.src} alt={cl.alt} className="modalSam"/>
 					<div className="modalDesc" dangerouslySetInnerHTML={{__html: cl.txt}} />
-				</div>)}</section>
+				</div>)}
+				</section>
 			//</>
 			)
 	}
--- src/data/clients.js
+++ src/data/clients.js
@@ -4,7 +4,7 @@ export const clients = [
   src : "./imgs/clients/client-placeholder-01.jpg",
   width : 1024,
   height: 717,
-  alt : "a",
+  alt : "",
   className : "client-narrow",
   txt : "bla"
  },
@@ -22,7 +22,7 @@ export const clients = [
   src : "./imgs/clients/client-placeholder-03.jpg",
   width : 1139,
   height: 1600,
-  alt : "a",
+  alt : "",
   className : "client-narrow",
   txt : "a"
  },
@@ -40,7 +40,7 @@ export const clients = [
   src : "./imgs/clients/client-placeholder-05.jpg",
   width : 1181,
   height: 1639,
-  alt : "a",
+  alt : "",
   className : "client-narrow",
   txt : "a"
  },
@@ -49,7 +49,7 @@ export const clients = [
   src : "./imgs/clients/client-placeholder-06.jpg",
   width : 1280,
   height: 1011,
-  alt : "a",
+  alt : "",
   className : "client-wide",
   txt : "<h2 class='modalNaslov'>tori amos</h2><div class='modalDiv'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At dignissimos iusto soluta quaerat numquam enim facilis laboriosam. Nemo atque dolor est cupiditate voluptatibus reiciendis ut nulla, vitae, ipsum laudantium ipsam!</div><div class='modalDiv'>Enim velit, repudiandae eum mollitia asperiores, suscipit praesentium quaerat sed iste quos ea accusantium rem voluptatem molestias possimus temporibus quasi earum quae inventore! Doloremque odio, voluptate dolorem, maiores quis officia!</div>"
  },
@@ -67,7 +67,7 @@ export const clients = [
   src : "./imgs/clients/client-placeholder-08.jpg",
   width : 1600,
   height: 900,
-  alt : "a",
+  alt : "",
   className : "client-wide",
   txt : "<h2 class='modalNaslov'>blondi</h2><div class='modalDiv'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At dignissimos iusto soluta quaerat numquam enim facilis laboriosam. Nemo atque dolor est cupiditate voluptatibus reiciendis ut nulla, vitae, ipsum laudantium ipsam!</div><div class='modalDiv'>Enim velit, repudiandae eum mollitia asperiores, suscipit praesentium quaerat sed iste quos ea accusantium rem voluptatem molestias possimus temporibus quasi earum quae inventore! Doloremque odio, voluptate dolorem, maiores quis officia!</div>"
  },
@@ -76,7 +76,7 @@ export const clients = [
   src : "./imgs/clients/client-placeholder-09.jpg",
   width : 1376,
   height: 1365,
-  alt : "a",
+  alt : "",
   className : "client-wide",
   txt : "<h2 class='modalNaslov'>čarli menson</h2><div class='modalDiv'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At dignissimos iusto soluta quaerat numquam enim facilis laboriosam. Nemo atque dolor est cupiditate voluptatibus reiciendis ut nulla, vitae, ipsum laudantium ipsam!</div><div class='modalDiv'>Enim velit, repudiandae eum mollitia asperiores, suscipit praesentium quaerat sed iste quos ea accusantium rem voluptatem molestias possimus temporibus quasi earum quae inventore! Doloremque odio, voluptate dolorem, maiores quis officia!</div>"
  },
@@ -85,7 +85,7 @@ export const clients = [
   src : "./imgs/clients/client-placeholder-10.jpg",
   width : 1065,
   height: 1600,
-  alt : "a",
+  alt : "",
   className : "client-narrow",
   txt : "a"
  },
@@ -94,7 +94,7 @@ export const clients = [
   src : "./imgs/clients/client-placeholder-11.jpg",
   width : 854,
   height: 1280,
-  alt : "a",
+  alt : "",
   className : "client-wide",
   txt : "<h2 class='modalNaslov'>štrafta</h2><div class='modalDiv'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At dignissimos iusto soluta quaerat numquam enim facilis laboriosam. Nemo atque dolor est cupiditate voluptatibus reiciendis ut nulla, vitae, ipsum laudantium ipsam!</div><div class='modalDiv'>Enim velit, repudiandae eum mollitia asperiores, suscipit praesentium quaerat sed iste quos ea accusantium rem voluptatem molestias possimus temporibus quasi earum quae inventore! Doloremque odio, voluptate dolorem, maiores quis officia!</div>"
  },
@@ -103,7 +103,7 @@ export const clients = [
   src : "./imgs/clients/client-placeholder-12.jpg",
   width : 800,
   height: 1200,
-  alt : "a",
+  alt : "",
   className : "client-narrow",
   txt : "a"
  },
