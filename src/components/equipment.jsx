994ed9f3a73780f231fdb34350a1e89b7e804120
IgorMilatovic <igby.veko@gmail.com>
2019-09-03 23:49:05 +0200

    equipment

--- src/components/equipment.jsx
+++ src/components/equipment.jsx
@@ -2,12 +2,55 @@ import React, { Component } from 'react';
 
 class Equipment extends Component {
 	state = {
-		content: ["Oprema", "Equipment"]
+		title: ["Oprema", "Equipment"],
+		text: [
+			[`OpremaSR: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet fugiat similique error impedit 
+			ut fuga nisi voluptate veritatis nihil quas ipsam vel sunt alias tempora laboriosam quia, ipsum 
+			ab assumenda aut laborum id animi iure. Reprehenderit quaerat architecto adipisci sunt autem impedit 
+			fugiat veritatis recusandae error, iste nisi, dignissimos distinctio consequatur vitae rem quos beatae 
+			illum eveniet? Aut dicta deleniti excepturi, blanditiis voluptatum, tempore? Voluptatibus provident, 
+			officia fugiat, est incidunt ad labore sequi necessitatibus perspiciatis, quis delectus. Reprehenderit quaerat 
+			architecto adipisci sunt autem impedit fugiat veritatis recusandae error, iste nisi, dignissimos distinctio 
+			consequatur vitae rem quos beatae illum eveniet? Aut dicta deleniti excepturi, blanditiis voluptatum, tempore? 
+			Voluptatibus provident, officia fugiat, est incidunt ad labore sequi necessitatibus perspiciatis, quis delectus.
+			`],
+
+			[`OpremaEN: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet fugiat similique error impedit 
+			ut fuga nisi voluptate veritatis nihil quas ipsam vel sunt alias tempora laboriosam quia, ipsum 
+			ab assumenda aut laborum id animi iure. Reprehenderit quaerat architecto adipisci sunt autem impedit 
+			fugiat veritatis recusandae error, iste nisi, dignissimos distinctio consequatur vitae rem quos beatae 
+			illum eveniet? Aut dicta deleniti excepturi, blanditiis voluptatum, tempore? Voluptatibus provident, 
+			officia fugiat, est incidunt ad labore sequi necessitatibus perspiciatis, quis delectus. Reprehenderit quaerat 
+			architecto adipisci sunt autem impedit fugiat veritatis recusandae error, iste nisi, dignissimos distinctio 
+			consequatur vitae rem quos beatae illum eveniet? Aut dicta deleniti excepturi, blanditiis voluptatum, tempore? 
+			Voluptatibus provident, officia fugiat, est incidunt ad labore sequi necessitatibus perspiciatis, quis delectus.`]
+		]
 	}
 	
 	render() {
+		const { title, text } = this.state;
 		let lang = this.props.lang;
-		return ( <section className="equipment" id="equipment">{this.state.content[lang]}</section> )
+		return ( 
+		<section className="equipment" id="equipment">
+			<div className="equipmentContentWrapper">
+				<div className="equipmentTxtWrapper">
+					<h2>{title[lang]}</h2>
+					<p>{text}</p>
+				</div>
+				<div className="equipmentImgsWrapper">
+					<div className="equipTopImgs">
+					<div className="inner1"><div className="equipImg1"></div></div>
+						<div className="equipImg2"></div>
+					</div>
+					<div className="equipBottomImgs">
+						<div className="equipImg3"></div>
+						<div className="equipImg4"></div>
+					</div>
+				</div>
+			</div>
+			
+		</section> 
+		)
 	}
 }
 export default Equipment;
